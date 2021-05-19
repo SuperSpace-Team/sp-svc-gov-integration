8d5d525091892b936c36a372f821a9588b60c790
陆超#81084998 <luchao1@yonghui.cn>
2021-05-19 15:32:40 +0800

    Updated init config for UT.

--- yh-infra-svc-gov-sdk/src/test/java/com.yh.infra.svc.gov.sdk/init/AppRegLauncherTest.java
+++ yh-infra-svc-gov-sdk/src/test/java/com.yh.infra.svc.gov.sdk/init/AppRegLauncherTest.java
@@ -2,6 +2,7 @@ package com.yh.infra.svc.gov.sdk.init;
 
 import com.yh.infra.svc.gov.sdk.command.AccessTokenCommand;
 import com.yh.infra.svc.gov.sdk.command.AccountAuthReturnObj;
+import com.yh.infra.svc.gov.sdk.command.BaseResponseEntity;
 import com.yh.infra.svc.gov.sdk.constant.SdkCommonConstant;
 import com.yh.infra.svc.gov.sdk.init.context.BeanRegistry;
 import com.yh.infra.svc.gov.sdk.init.daemon.VersionChecker;
@@ -35,44 +36,42 @@ public class AppRegLauncherTest {
 	public void setUp() throws Exception {
 		MockitoAnnotations.initMocks(this);
 		client = new AppRegLauncher();
-		
-		client.setAppKey("UT-APP1");
-		client.setAppSecret("12345678");
-		client.setUnionGatewayUrl("http://yh-infra-union-gateway.devgw.yonghui.cn");
+		client.setAppKey("demo--yh-test-svc");
+		client.setAppSecret("rPuKYUvnb6xYGSqXOzhwd7IDU1WaeKQc");
+		client.setUnionGatewayUrl("http://yh-infra-union-gateway-dev.yonghui.cn");
 		client.setHttpClientProxy(httpClient);
 		client.setEnabled(true);
 		
 		BeanRegistry sc = BeanRegistry.getInstance();
 		sc.register(SdkCommonConstant.SDK_INITIALIZED_FLAG, false);
-		
 		sc.register(HttpClientProxy.class.getName(), httpClient);
 
 		Map<String, String> respMap = new HashMap<String, String>();
 		String respStr;
 
-		AccountAuthReturnObj aaro = new AccountAuthReturnObj();
-		aaro.setResultFlag(true);
-		aaro.setData("TOKEN123");
+		BaseResponseEntity aaro = new BaseResponseEntity();
+		aaro.setIsSuccess(true);
+		aaro.setData("svc-gov-app-token-426161524495749120");
 		respStr = JsonUtil.writeValue(aaro);
 		respMap.put("result", respStr);
 		respMap.put("status", "200");
-		when(httpClient.postJson(eq("http://yh-infra-union-gateway-dev.devgw.yonghui.cn/svc-gov/app/getAppAuthCode"),
+		when(httpClient.postJson(eq("http://yh-infra-union-gateway-dev.yonghui.cn/svc-gov/app/getAppAuthCode"),
 				anyString(), anyInt(), any(Header[].class))).thenReturn(respMap);
 
 		AccessTokenCommand token = new AccessTokenCommand();
-		respMap = new HashMap<String, String>();
-		token = new AccessTokenCommand();
-		token.setAccessToken("TOKEN123");
-		token.setExpireTime(312312312l);
+		token.setAccessToken("svc-gov-app-token-426161524495749120");
+		token.setExpireTime(85652463L);
 		respStr = JsonUtil.writeValue(token);
 
-		aaro = new AccountAuthReturnObj();
-		aaro.setResultFlag(true);
+		aaro = new BaseResponseEntity();
+		aaro.setIsSuccess(true);
 		aaro.setData(respStr);
 		respStr = JsonUtil.writeValue(aaro);
+
+		respMap = new HashMap();
 		respMap.put("result", respStr);
 		respMap.put("status", "200");
-		when(httpClient.postJson(eq("http://yh-infra-union-gateway.devgw.yonghui.cn/svc-gov/app/login"),
+		when(httpClient.postJson(eq("http://yh-infra-union-gateway-dev.yonghui.cn/svc-gov/app/login"),
 				anyString(), anyInt(), any(Header[].class))).thenReturn(respMap);
 	}
 
