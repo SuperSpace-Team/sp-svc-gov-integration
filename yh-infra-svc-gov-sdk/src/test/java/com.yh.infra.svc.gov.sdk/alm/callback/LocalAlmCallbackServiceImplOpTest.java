4310a178a447e450d280dfad9736f756841dc837
zhiyuanqin <80961464@yonghui.cn>
2021-05-26 10:17:05 +0800

    json调整

--- yh-infra-svc-gov-sdk/src/test/java/com.yh.infra.svc.gov.sdk/alm/callback/LocalAlmCallbackServiceImplOpTest.java
+++ yh-infra-svc-gov-sdk/src/test/java/com.yh.infra.svc.gov.sdk/alm/callback/LocalAlmCallbackServiceImplOpTest.java
@@ -1,13 +1,11 @@
 package com.yh.infra.svc.gov.sdk.alm.callback;
 
-import com.yh.infra.svc.gov.agent.agent.AgentBeanRegistry;
 import com.yh.infra.svc.gov.agent.agent.AgentInstallProcessor;
 import com.yh.infra.svc.gov.sdk.alm.callback.impl.LocalAlmCallbackServiceImpl;
 import com.yh.infra.svc.gov.sdk.alm.config.AlmConfig;
 import com.yh.infra.svc.gov.sdk.alm.context.MonitorGlobalContext;
 import com.yh.infra.svc.gov.sdk.command.cfg.AppConfig;
 import com.yh.infra.svc.gov.sdk.config.AppRegConfig;
-import com.yh.infra.svc.gov.sdk.constant.SdkCommonConstant;
 import com.yh.infra.svc.gov.sdk.constant.SdkCommonConstant;
 import com.yh.infra.svc.gov.sdk.init.command.VersionQueryResp;
 import com.yh.infra.svc.gov.sdk.init.context.BeanRegistry;
@@ -32,15 +30,12 @@ import static org.junit.Assert.assertEquals;
  */
 @Ignore
 public class LocalAlmCallbackServiceImplOpTest {
-
-    private LocalAlmCallbackServiceImpl service = null;
+    private LocalAlmCallbackServiceImpl service;
     private MonitorGlobalContext context;
-    
-    
+
     @Mock
     AgentInstallProcessor mockProcessor;
-    
-    
+
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
@@ -51,9 +46,7 @@ public class LocalAlmCallbackServiceImplOpTest {
         
         BeanRegistry.getInstance().register(context);
         BeanRegistry.getInstance().register(SdkCommonConstant.ALM_INITIALIZED_FLAG, true);
-        
-        
-        AgentBeanRegistry.register(AgentInstallProcessor.class.getName(), mockProcessor);
+        BeanRegistry.getInstance().register(AgentInstallProcessor.class.getName(), mockProcessor);
     }
 
     @After
@@ -68,7 +61,7 @@ public class LocalAlmCallbackServiceImplOpTest {
         assertEquals(-1 , context.getCurrentVersion());
     }
     private AppConfig createAc() {
-    	String json = "{\"appId\":\"IOSP-OR-ST\",\"version\":58,\"lifecycle\":true,\"entries\":[{\"code\":22,\"className\":\"com.baozun.scm.primservice.orc.manager.inter.OrderSplitServiceManagerImpl\",\"methodName\":\"receiveOrderInfoListByMq\",\"inputParamType\":\"List\",\"description\":\"从mq中消费上游系统的订单\"}],\"factors\":[{\"code\":13,\"bizAlias\":\"RL-ORDER\",\"name\":\"魔镜-RL官网订单发货流程\",\"description\":\"RL官网订单发货流程\",\"volume\":2,\"clientMode\":2,\"serverMode\":2}],\"logCfgs\":[],\"nodes\":[{\"code\":16,\"bizCode\":13,\"entryCode\":22,\"seq\":141,\"seqMatchExp\":null,\"matchExp\":null,\"keyExp\":\"#IN[P1].![#this.platformCode]\",\"keyTransAlias\":\"UNEX-CODE\",\"inLogExp\":\"'IOSP将路由结果反馈至HUB3'\",\"outLogExp\":null,\"errorLogExp\":null,\"threshold\":null}],\"transformNodes\":[],\"logTargets\":[{\"bizCode\":13,\"logFile\":false,\"logArch\":true,\"level\":\"INFO\"}],\"commonCfgs\":[{\"cfgKey\":\"SYS_LOG_ARCH\",\"cfgValue\":\"http://10.101.6.66:1205/api/log/monitorData\"}]}";
+    	String json = "{\"appId\":\"IOSP-OR-ST\",\"version\":58,\"lifecycle\":true,\"entries\":[{\"code\":22,\"className\":\"com.baozun.scm.primservice.orc.manager.inter.OrderSplitServiceManagerImpl\",\"methodName\":\"receiveOrderInfoListByMq\",\"inputParamType\":\"List\",\"description\":\"从mq中消费上游系统的订单\"}],\"monitor\":{\"factors\":[{\"code\":13,\"bizAlias\":\"RL-ORDER\",\"name\":\"魔镜-RL官网订单发货流程\",\"description\":\"RL官网订单发货流程\",\"volume\":2,\"clientMode\":2,\"serverMode\":2}],\"logCfgs\":[],\"nodes\":[{\"code\":16,\"bizCode\":13,\"entryCode\":22,\"seq\":141,\"seqMatchExp\":null,\"matchExp\":null,\"keyExp\":\"#IN[P1].![#this.platformCode]\",\"keyTransAlias\":\"UNEX-CODE\",\"inLogExp\":\"'IOSP将路由结果反馈至HUB3'\",\"outLogExp\":null,\"errorLogExp\":null,\"threshold\":null}],\"transformNodes\":[],\"logTargets\":[{\"bizCode\":13,\"logFile\":false,\"logArch\":true,\"level\":\"INFO\"}]},\"commonCfgs\":[{\"cfgKey\":\"SYS_LOG_ARCH\",\"cfgValue\":\"http://10.101.6.66:1205/api/log/monitorData\"}]}";
     	return JsonUtil.readValueSafe(json, AppConfig.class);
     }
 
